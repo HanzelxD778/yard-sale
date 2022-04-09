67f04d276ceef41a7778c18db52eca3a1849958f
Hanzel Urrutia <hanzelurrutia8118@gmail.com>
2022-04-09 13:44:35 -0600

    Agregando uso de inputs del padre al hijo para las imagenes

---
+++ yard-sale/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  imgParent = 'https://c.tenor.com/GZyF7v0Z-bwAAAAC/yuna-yuna-itzy.gif';
+}
